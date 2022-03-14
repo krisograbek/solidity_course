6c3cfe6ac1ccc99e907af571e6e610dcbf435e36
krisograbek <krzysztof.ograbek@gmail.com>
2022-03-14 09:15:11 +0100

    finish get_contract and deploy_mocks, add Mock for price feeds

--- defi-stake-yield/scripts/deploy_contracts.py
+++ defi-stake-yield/scripts/deploy_contracts.py
@@ -1,10 +1,53 @@
-from brownie import DappToken, TokenFarm, config
-from scripts.helpful_scripts import get_account
+from brownie import DappToken, TokenFarm, config, network
+from scripts.helpful_scripts import get_account, get_contract
+from web3 import Web3
+
+KEPT_BALANCE = Web3.toWei(100, "ether")
 
 
 def deploy_contracts():
     account = get_account()
     dapp_token = DappToken.deploy({"from": account})
+    token_farm = TokenFarm.deploy(
+        dapp_token.address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    # transfer dapp tokens to our token farm
+    tx = dapp_token.transfer(
+        token_farm.address, dapp_token.totalSupply() - KEPT_BALANCE, {"from": account}
+    )
+    tx.wait(1)
+
+    weth_token = get_contract("weth_token")
+    dai_token = get_contract("dai_token")
+
+    # we're using the dict so, that the key is an actual token contract
+    # and the value is a price feed contract
+    dict_of_allowed_tokens = {
+        dapp_token: get_contract("dai_usd_price_feed"),
+        dai_token: get_contract("dai_usd_price_feed"),
+        weth_token: get_contract("eth_usd_price_feed"),
+    }
+
+    add_allowed_tokens(token_farm, dict_of_allowed_tokens, account)
+
+
+def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
+    # loop through allowed tokens
+    # key - token
+    # value - price feed for the token
+    for token in dict_of_allowed_tokens:
+        # add the token address to allowed tokens
+        add_tx = token_farm.addAllowedTokens(token.address, {"from": account})
+        add_tx.wait(1)
+        # map tokens with their price feeds
+        set_tx = token_farm.setPriceFeedContract(
+            token.address, dict_of_allowed_tokens[token].address, {"from": account}
+        )
+        set_tx.wait(1)
+
+    return token_farm
 
 
 def main():
--- defi-stake-yield/scripts/helpful_scripts.py
+++ defi-stake-yield/scripts/helpful_scripts.py
@@ -1,8 +1,19 @@
-from brownie import Contract, config, network, accounts, MockWETH, MockDAI
+from brownie import (
+    Contract,
+    config,
+    network,
+    accounts,
+    MockWETH,
+    MockDAI,
+    MockV3Aggregator,
+)
 
 
 LOCAL_BLOCKCHAINS = ["development", "mainnet-fork"]
 TEST_NETWORKS = ["rinkeby", "kovan"]
+
+DECIMALS = 18
+INITIAL_PRICE_FEED_VALUE = 2500000000000000000000  # 2500 * 10 ** 18
 
 
 def get_account(index=None):
@@ -18,10 +29,24 @@ def get_account(index=None):
 contract_to_mock = {
     "weth_token": MockWETH,
     "dai_token": MockDAI,
+    "eth_usd_price_feed": MockV3Aggregator,
+    "dai_usd_price_feed": MockV3Aggregator,
 }
 
 
 def get_contract(contract_name):
+    """
+    This function will either:
+        - Get an address from the config
+        - Or deploy a Mock to use for a network that doesn't have the contract
+    Args:
+        contract_name (string): This is the name of the contract that we will get
+        from the config or deploy
+    Returns:
+        brownie.network.contract.ProjectContract: This is the most recently deployed
+        Contract of the type specified by a dictionary. This could either be a mock
+        or a 'real' contract on a live network.
+    """
     contract_type = contract_to_mock[contract_name]
     if network.show_active() in LOCAL_BLOCKCHAINS:
         if len(contract_type) <= 0:
@@ -40,5 +65,22 @@ def get_contract(contract_name):
     return contract
 
 
-def deploy_mocks():
-    pass
+def deploy_mocks(decimals=DECIMALS, initial_price_feed=INITIAL_PRICE_FEED_VALUE):
+    """
+    Deploy mocks for local Blockchains
+    """
+    account = get_account()
+    print(f"The current active network is {network.show_active()}")
+
+    print("Deploying AggregatorV3...")
+    mock_price_feed = MockV3Aggregator.deploy(
+        decimals, initial_price_feed, {"from": account}
+    )
+    print(f"Deployed AggregatorV3 to {mock_price_feed.address} ")
+
+    print("Deploying DAI Token...")
+    dai_token = MockDAI.deploy({"from": account})
+    print(f"Deployed DAI Token to {dai_token.address} ")
+    print("Deploying WETH Token...")
+    weth_token = MockWETH.deploy({"from": account})
+    print(f"Deployed WETH Token to {weth_token.address} ")
